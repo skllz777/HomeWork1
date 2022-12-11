19eaef82b672ec2a7552844f3e48e42cbbcf4e11
Oleg <tradecenter1771@gmail.com>
2022-12-11 13:49:54 +0300

    add Tasks

---
+++ Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1-20, 20);
+}
+
+
+void ReleaseArray(int[] array)
+{
+    int numbers = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+            numbers++;
+        
+    }
+    Console.WriteLine($"Number of positive numbers in the array: {numbers}");
+}
+
+Console.Clear();
+Console.Write("Enter the number of elements: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+ReleaseArray(array);
---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 
+Console.Write("Enter k1: ");
+var k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter b1: ");
+var b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter k2: ");
+var k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter b2: ");
+var b2 = Convert.ToDouble(Console.ReadLine());
+ 
+ 
+var x = -(b1 - b2) / (k1 - k2);
+var y = k1 * x + b1;
+ 
+
+ 
+Console.WriteLine($"Intersection at a point: ({x};{y})");
